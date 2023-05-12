3159a1d90c402f10a31e4047d14a3cd4cac4c290
zak-soussi <92606256+zak-soussi@users.noreply.github.com>
2023-05-12 09:44:29 +0100

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1809B | Points on Plane/198828736.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+const char nl='\n';
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        ll q = sqrtl (n - 1);
+        cout << q << '\n';
+    }
+    return 0;
+}
+ 
+ 
