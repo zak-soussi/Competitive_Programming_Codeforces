b2fd0ca51360f4b44ae1e4a2d2ae406915187fd8
zak-soussi <92606256+zak-soussi@users.noreply.github.com>
2023-05-12 09:42:42 +0100

    Committing files

---
+++ CodeForces/GNU C++20 (64)/490A | Team Olympiad/202396001.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+typedef long long ll;
+typedef long double ld;
+const char nl='\n';
+const int infiniint=INT_MAX;
+const ll infinill=LONG_LONG_MAX;
+using namespace std;
+ll power_no_mod(ll a, ll b) {
+    ll result = 1;
+    while (b) {
+        if (b&1) result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
+ll power_with_mod(ll a, ll b,ll m) {
+    ll result = 1;
+    while (b) {
+        if (b&1) result =(result*a)%m;
+        a = (a*a)%m;
+        b /= 2;
+    }
+    return result;
+}
+template <typename  type>
+struct mycomp {
+    bool operator()(const type & first,const type & second) const{
+        /*
+        write whatever code you want here in order to have the wanted sorting logic,
+        for exp return first<second; : this will work as by default sorting(increasing order),
+         you can use first.first(if you are working with pair),
+         when using this function in a structure, write: mycomp<type that it will work with>
+         */
+    }
+};
+void coordinate_compress(vector<int> &initial, vector<int> &iToV, map<int , int> &vToI, int start =0 , int step =1){
+    /*
+    nst3mlouha bch ncompressiw vecteur mt3na,m3neha f 3oudh mat5dm 3ala vecteur kobro 10 7 w tjik mle,
+    n5dhou ken les indices eli 7chtna behom w nsortiwhom w nsn3ou mnhom tab s4ir fih des indices s4ar
+    en relation b les indices l7a9aniyin
+    initial yod5l b les valeurs mahomch mratbin ma3ndhom 7ata 3ala9a w yo5rj b les indices mt3 les valeurs edhoukom
+    f tab jdid
+    */
+    for (auto &x_pos : initial)
+        vToI[x_pos]=0;
+    iToV.resize(start + step * vToI.size());
+    int idx = 0;
+    for (auto &entry : vToI)
+    {
+        entry .second =start + step *idx;
+        iToV [entry .second ] =entry.first;
+        ++idx;
+    }
+    for (auto &x_pos : initial)
+        x_pos =vToI[x_pos];
+}
+bool isPrime(ll n) {
+    if (n == 1) return false;
+    if (n == 2) return true;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+    int n;
+    cin>>n;
+    int total=0;
+    vector<int>res(3*(n+1),0);
+    int b;
+    int onecomp=0,twocomp=0,threecomp=0;
+    for (int i = 1; i <= n; ++i) {
+        cin>>b;
+        if(b==1)
+        {
+            res[3*onecomp]=i;
+            onecomp++;
+        }
+        else if(b==2)
+        {
+            res[3*twocomp+1]=i;
+            twocomp++;
+        }
+        else
+        {
+            res[3*threecomp+2]=i;
+            threecomp++;
+        }
+    }
+    for (int i = 0; i < res.size(); i+=3) {
+        if(!res[i]||!res[i+1]||!res[i+2])
+            break;
+        total++;
+    }
+    cout<<total<<nl;
+    for (int i = 0; i < res.size(); i+=3) {
+        if(!res[i]||!res[i+1]||!res[i+2])
+            break;
+        cout<<res[i]<<" "<<res[i+1]<<" "<<res[i+2]<<nl;
+    }
+    return 0;
+}
